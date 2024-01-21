15e6d96d175bbafa77c4bb50bc7c20a889b7023e
Matthew Chesser <chesser.matthew@gmail.com>
2024-01-21 10:03:35 -0500

    downloader.py documented

--- downloader.py
+++ downloader.py
@@ -6,7 +6,16 @@ from utils import load_secrets, write_json
 
 def get_messages(channel_id:str, num_messages:int=100, before_message_id:str=None, headers:dict=None) -> dict:
     """
-    Scrapes messages
+    Scrapes messages from a discord channel
+
+    Input:
+        - channel_id: The channel ID of the channel to scrape.
+        - num_messages: How many messages to request.
+        - before_message_id: Message ID of the last message scraped (for pagination)
+        - headers: Dictionary of header information. 
+    
+    Output:
+        - res_json: The JSON containing the message response from the discord API.
     """
 
     if (before_message_id is None):
@@ -19,7 +28,13 @@ def get_messages(channel_id:str, num_messages:int=100, before_message_id:str=Non
 
 def parse_message(res_message:dict) -> dict:
     """
-    Slims down message response into fields we care about
+    Slims down message response into fields we care about.
+
+    Input:
+        - res_message: An individual response message as a dictionary. 
+    
+    Output:
+        - message: The same input message, just slimmed down and slightly reformatted.
     """
     message = {}
     message["id"] = res_message["id"]
@@ -49,32 +64,30 @@ def main():
     channel_id = secrets["channels"]["main"]
     headers = secrets["headers"]
 
-
-    # Get first batch of jsons
     final_out_json = []
     message_id = None 
 
     iter_count = 0
     try:
         while True:
+            # Sleep one second per 100 messages. Proactive rate limiting.
             time.sleep(1)
 
-            # Get the messages
-            out_json = get_messages(channel_id, before_message_id=message_id, headers=headers)
-
-            # Last message id (used for pagination on next request above)
-            message_id = out_json[-1]["id"]
-            
+            # Scrape 100 messages from discord API, paginating after last message.
+            out_json = get_messages(channel_id, before_message_id=message_id, headers=headers)            
             for res_message in out_json:
                 final_out_json.append(parse_message(res_message))
             
+            # Last message id (used for pagination on next request above)
+            message_id = out_json[-1]["id"]
+
             iter_count += 1
             if (iter_count % 100 == 0):
                 print(f"Finished iteration: {iter_count}")
     except Exception as e:
         print(f"caught exception: {e}")
     finally:
-        write_json(final_out_json, "messages.json")
+        write_json(final_out_json, "messages/messages.json")
 
 
 if __name__ == "__main__":
