c5e88899922cf5befccced4154234ddc47d0932b
Matthew Chesser <chesser.matthew@gmail.com>
2024-01-21 10:25:01 -0500

    Doocumented utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,11 @@
 # utils.py
 import json 
 
+# Custom tokens
 SEP = "[SEP]"
 PAD = "[PAD]"
 BOS = "[BOS]"
 EOS = "[EOS]"
-LOWER_CASE = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-NUMBERS = [str(x) for x in range(0, 10)]
-PUNCUATION_OTHER = [" ", ".", "?",  ",", "'", "!", "@", '%', '\n', ':', ';']
 IMG_TOKEN = "[IMG]"
 GIF_TOKEN = "[GIF]"
 LINK_TOKEN = "[LINK]"
@@ -20,6 +18,12 @@ with open("custom/emotes.txt", 'r') as fp:
 def read_json(json_filename : str) -> dict:
     """
     JSON read helper
+
+    Input:
+        - json_filename: Path to JSON filename
+    
+    Output:
+        - json_obj: Dictionary of JSON contents
     """
     with open(json_filename, 'r')  as fp:
         file_data = fp.read()
@@ -29,6 +33,13 @@ def read_json(json_filename : str) -> dict:
 def write_json(json_dict : dict, out_file : str) -> None:
     """
     JSON write helper
+
+    Input:
+        - json_dict: Python dictionary representing JSON contents.
+        - out_file: Filename to write json_dict contents to.
+    
+    Output:
+        - None. (writes to file)
     """
     with open(out_file, 'w') as out_fp:
         out_fp.write(json.dumps(json_dict, indent=4))
@@ -39,6 +50,12 @@ def write_json(json_dict : dict, out_file : str) -> None:
 def read_jsonl(filename : str) -> list:
     """
     Reads .jsonl file
+
+    Input:
+        - filename: Filename of .jsonl file to read.
+    
+    Output:
+        - jsonl_data : List of dictionaries representing .jsonL data
     """
 
     with open (filename, 'r') as fp:
@@ -50,6 +67,15 @@ def read_jsonl(filename : str) -> list:
 def load_secrets(secret_filename:str="custom/secrets.json") -> dict:
     """
     Helper to load the secrets file. 
+    
+    Created as a separate function for clarity and so users can 
+    modify contents in case secrets file is not JSON.
+
+    Input:
+        - secret_filename: Filename of the secrets file.
+    
+    Output:
+        - secrets: Secrets as python dictionary.
     """
     secrets = read_json(secret_filename)
 
